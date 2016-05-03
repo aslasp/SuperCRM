458ce2334c10e25692f94ae76d2848d4b3c4ac6f
aswasn <wn13nju@163.com>
2016-05-03 19:06:29 +0800

    Login界面

---
+++ SuperCRM/app/src/main/java/wn13/supercrm/LoginActivity.java
@@ -0,0 +1,17 @@
+package wn13.supercrm;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.Button;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
