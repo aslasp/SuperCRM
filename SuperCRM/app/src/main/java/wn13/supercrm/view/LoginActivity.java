05f6850f2d94cb15367a65be995f5b3a9ac96950
aswasn <wn13nju@163.com>
2016-05-04 16:06:27 +0800

    main

---
+++ SuperCRM/app/src/main/java/wn13/supercrm/view/LoginActivity.java
@@ -0,0 +1,45 @@
+package wn13.supercrm.view;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import wn13.supercrm.R;
+
+public class LoginActivity extends Activity {
+
+    EditText idFld,pswdFld;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        idFld=(EditText) findViewById(R.id.login_idFld);
+        pswdFld=(EditText) findViewById(R.id.login_pswdFld);
+
+        //为登录按钮增加监听
+        Button loginBtn=(Button)findViewById(R.id.login_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginCheck();
+            }
+        });
+    }
+
+    private void loginCheck(){
+        String id=idFld.getText().toString();
+        String pswd=pswdFld.getText().toString();
+        if((!id.equals(""))&&(!pswd.equals(""))){
+            Intent intent=new Intent();
+            intent.putExtra("id",id);
+            intent.putExtra("pswd",pswd);
+            intent.setClass(LoginActivity.this,MainActivity.class);
+            startActivity(intent);
+        }
+    }
+}
--- SuperCRM/app/src/main/java/wn13/supercrm/view/MainActivity.java
+++ SuperCRM/app/src/main/java/wn13/supercrm/view/MainActivity.java
@@ -1,7 +1,10 @@
-package wn13.supercrm;
+package wn13.supercrm.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import wn13.supercrm.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
