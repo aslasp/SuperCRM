766863810dc89c38fb6fc40dd3f479cde516c002
aswasn <wn13nju@163.com>
2016-05-24 22:00:22 +0800

    hehe,添加交易会闪退

--- SuperCRM/app/src/main/java/wn13/supercrm/view/schedule/AddScheduleActivity.java
+++ SuperCRM/app/src/main/java/wn13/supercrm/view/schedule/AddScheduleActivity.java
@@ -1,7 +1,17 @@
 package wn13.supercrm.view.schedule;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 import wn13.supercrm.R;
 
@@ -11,5 +21,64 @@ public class AddScheduleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_schedule);
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setTitle("添加日程");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        //为日期和时间标签加监听
+        setupDateTextView();
+        setupTimeTextView();
+
+        //为下方返回按钮加监听
+        setupBackBtn();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:onBackPressed();break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDateTextView(){
+        findViewById(R.id.addScheduleDateTextView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal=Calendar.getInstance();
+                new DatePickerDialog(AddScheduleActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        TextView dView=(TextView)findViewById(R.id.addScheduleDateTextView);
+                        dView.setText(year+"-"+(monthOfYear+1)+"-"+dayOfMonth);
+                    }
+                }, cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),cal.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    private void setupTimeTextView(){
+        findViewById(R.id.addScheduleTimeTextView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar c=Calendar.getInstance();
+                new TimePickerDialog(AddScheduleActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        ((TextView)findViewById(R.id.addScheduleTimeTextView)).setText(hourOfDay+":"+minute);
+                    }
+                },c.get(Calendar.HOUR_OF_DAY),c.get(Calendar.MINUTE),true).show();
+            }
+        });
+    }
+
+    private void setupBackBtn(){
+        findViewById(R.id.addScheduleCloseBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
--- SuperCRM/app/src/main/java/wn13/supercrm/view/trade/AddTradeActivity.java
+++ SuperCRM/app/src/main/java/wn13/supercrm/view/trade/AddTradeActivity.java
@@ -1,7 +1,10 @@
 package wn13.supercrm.view.trade;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 import wn13.supercrm.R;
 
@@ -11,5 +14,31 @@ public class AddTradeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_trade);
+
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setTitle("添加商机");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+
+        //为下方返回按钮加监听
+        setupBackBtn();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:onBackPressed();break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupBackBtn(){
+        findViewById(R.id.addCustomerCloseBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
